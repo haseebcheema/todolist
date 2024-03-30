8e39e1158b1914817339d83650010cde164db2ed
haseebcheema <ihaseebtariq1@gmail.com>
2024-03-31 00:21:20 +0400

    finish setup

--- app/Components/form.js
+++ app/Components/form.js
@@ -5,18 +5,46 @@ import React, { useState } from 'react';
 export default function Form() {
     
     const [task, setTask] = useState('');
+    const [tasksList, setTaskList] = useState([]);
+
     const handleChange = function(e){
         setTask(e.target.value);
     }
     const submitHandler = function(e){
         e.preventDefault();
+        setTaskList([...tasksList, task]);
+        console.log(tasksList);
         setTask('');
+    }
+    const deleteHandler = function(i){
+      let copyTasks = [...tasksList];
+      copyTasks.splice(i, 1);
+      setTaskList(copyTasks);
+    }
+
+    let renderTasks = 'You are free today!'
+    if(tasksList.length>0){
+        renderTasks = tasksList.map((t, index)=>{
+        return(
+          <ul>
+            <li className='ml-1 flex justify-between w-11/12 '>
+              <h1 className='text-2xl mb-2'>{t}</h1>
+              <button className='text-white p-4 flex justify-center items-center rounded-full w-6 h-6 bg-indigo-500 hover:bg-indigo-600' type="button" onClick={()=>{
+                  deleteHandler(index);
+                }}>X</button>
+            </li>
+          </ul>
+        )
+      });
     }
 
   return (
-    <form onSubmit={submitHandler}>
-        <input className='bg-slate-100 p-4 rounded-full w-3/6' type="text" value={task} onChange={handleChange} placeholder='Add your task'/>
-        <button className='bg-red-500 rounded-full p-4 -ml-10 w-1/6' type="button">ADD</button>
-    </form>
+    <>
+      <form onSubmit={submitHandler} className='mb-4'>
+        <input className='bg-slate-100 p-4 rounded-full w-5/6' type="text" value={task} onChange={handleChange} placeholder='Add your task'/>
+        <button className='bg-indigo-500 rounded-full p-4 -ml-10 w-1/4 text-white font-semibold hover:bg-indigo-600' type="button">ADD</button>
+      </form>
+      {renderTasks}
+    </>
   )
 }
--- app/page.js
+++ app/page.js
@@ -4,9 +4,9 @@ import Form from './Components/form';
 export default function Home() {
   return (
     <>
-      <div className='w-screen h-screen bg-blue-600 flex justify-center items-center'>
-        <div className='bg-white w-6/12 h-5/6 border-0 rounded-lg p-10'>
-          <h1 className='w-fit text-2xl font-bold mb-2'>To-Do List</h1>
+      <div className='w-screen h-screen bg-gradient-to-r from-indigo-800 flex justify-start items-center'>
+        <div className='bg-white w-1/3 h-4/5 border-0 rounded-lg p-10 ml-28 overflow-auto'>
+          <h1 className='w-fit text-2xl font-bold mb-4'>To-Do List</h1>
           <Form/>
         </div>
       </div>
